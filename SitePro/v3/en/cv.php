56d2b251bbe689e8aeea257a4ef685a17e5e849e
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2024-10-09 11:27:04 +0200

    feat: ajout contenu CV + forme

--- SitePro/v3/en/cv.php
+++ SitePro/v3/en/cv.php
@@ -3,9 +3,54 @@
     <section>
         <h3>Antoine Poirier</h3>
         <p>
-            Student at IMT Nord Europe
-            <br>
-            I am looking for an internship in the field of web development lasting 16 weeks starting April 7, 2025.
+        Second year engineering student at IMT Nord Europe, I am looking for a 16-week internship in the digital field, particularly in the field of software engineering and information systems, at from April 7, 2025.
         </p>
     </section> 
+    <section class="conteneur_informations">
+        <section class="experience">
+            <h3>Experiences</h3>
+            <ul>
+                <li>June 2024 - August 2024</li>
+                <b>Actemium Paris Transport, Nanterre - IT development</b>
+                <br>
+                <i>1st year engineering cycle internship</i>
+                <br><br>
+                <li>October 2023 - May 2024</li>
+                <b>Project “Potag’Écolo”, Douai - Secretary</b>
+                <br>
+                <i>First year engineering cycle project</i>
+            </ul>
+        </section>
+        <section class="formation">
+            <h3>Education</h3>
+            <ul>
+                <li>2023 - Today</li>
+                <b>IMT Nord Europe, Douai</b>
+                <br>
+                <i>Second year of the engineering course</i>
+                <br><br>
+                <li>2021 - 2023</li>
+                <b>Lycée Chateaubriand, Rennes</b>
+                <br>
+                <i>Preparatory class for Grandes Ecoles</i>
+            </ul>
+        </section>
+    </section>
+    <div class="spacer"></div>   
+    <section class="competence">
+        <h3>Skills</h3>
+        <ul>
+            <li>Java, JavaScript, ExtJS, Python, C, Pharo, SQL, PostgreSQL, HTML, CSS, PHP</li>
+            <li>Github</li>
+            <li>Suite Office</li>
+        </ul>
+    </section>
+    <section class="langue">
+        <h3>Languages</h3>
+        <ul>
+            <li>French (native)</li>
+            <li>English (fluent)</li>
+            <li>Spanish (working knowledge)</li>
+        </ul>
+    </section>
 </div>
--- SitePro/v3/fr/cv.php
+++ SitePro/v3/fr/cv.php
@@ -1,11 +1,55 @@
 <div class="content">
     <h2>Mon CV</h2>
-    <section>
+    <section class="presentation">
         <h3>Antoine Poirier</h3>
         <p>
-            Étudiant à IMT Nord Europe
-            <br>
-            Je suis à la recherche d'un stage dans le domaine du développement web d'une durée de 16 semaines à partir du 7 avril 2025.
+            Étudiant en deuxième année de cycle ingénieur à IMT Nord Europe, je suis à la recherche d’un stage d’une durée de 16 semaines dans le domaine du numérique, en particulier dans le domaine du génie logiciel et des systèmes d’informations, à partir du 7 avril 2025.
         </p>
-    </section> 
+    </section>
+    <section class="conteneur_informations">
+        <section class="experience">
+            <h3>Expériences</h3>
+            <ul>
+                <li>Juin 2024 - Août 2024</li>
+                <b>Actemium Paris Transport, Nanterre - Développement informatique</b>
+                <br>
+                <i>Stage de 1ère année de cycle ingénieur</i>
+                <br><br>
+                <li>Octobre 2023 - Mai 2024</li>
+                <b>Projet “Potag’Écolo”, Douai - Secrétaire</b>
+                <br>
+                <i>Projet de première année de cycle ingénieur</i>
+            </ul>
+        </section>
+        <section class="formation">
+            <h3>Formation</h3>
+            <ul>
+                <li>2023 - Aujourd’hui</li>
+                <b>IMT Nord Europe, Douai</b>
+                <br>
+                <i>Deuxième année du cursus ingénieur</i>
+                <br><br>
+                <li>2021 - 2023</li>
+                <b>Lycée Chateaubriand, Rennes</b>
+                <br>
+                <i>Classe préparatoire aux grandes écoles</i>
+            </ul>
+        </section>
+    </section>
+    <div class="spacer"></div>   
+    <section class="competence">
+        <h3>Compétences</h3>
+        <ul>
+            <li>Java, JavaScript, ExtJS, Python, C, Pharo, SQL, PostgreSQL, HTML, CSS, PHP</li>
+            <li>Github</li>
+            <li>Suite Office</li>
+        </ul>
+    </section>
+    <section class="langue">
+        <h3>Langues</h3>
+        <ul>
+            <li>Anglais (C1, 985 TOEIC)</li>
+            <li>Espagnol (B1)</li>
+        </ul>
+    </section>
 </div>
