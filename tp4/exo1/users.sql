260de71b7ac53b6f6fc7b8a9f837405d6396999f
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2024-10-14 11:26:25 +0200

    feat: exo1 et 2 tp4

---
+++ tp4/exo1/users.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 14 oct. 2024 à 08:47
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `dbtest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `email`) VALUES
+(1, 'riri', 'riri@disney.com'),
+(2, 'fifi', 'fifi@disney.com'),
+(3, 'loulou', 'loulou@disney.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tp4/exo2/users.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once('config.php');
+
+$connectionString = "mysql:host=". _MYSQL_HOST;
+
+if(defined('_MYSQL_PORT'))
+    $connectionString .= ";port=". _MYSQL_PORT;
+
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
+
+$pdo = NULL;
+try {
+    $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $erreur) {
+    echo 'Erreur : '.$erreur->getMessage();
+}
+
+$request = $pdo->prepare("select * from users");
+
+// TODO: add your code here
+// retrieve data from database using fetch(PDO::FETCH_OBJ) and
+// display them in HTML array
+$request->execute();
+$data = $request->fetch(PDO::FETCH_OBJ);
+
+echo '<h1>Users</h1><ul>';
+echo '<table>  <tbody>';
+echo '
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">login</th>
+      <th scope="col">email</th>
+    </tr>
+  </thead>';
+while(!empty($data)) {
+    echo '<tr>';
+    echo '<th scope="row">'.$data->id.'</th><td>'.$data->login.'</td><td>'.$data->email.'</td>';
+    echo '</tr>';
+    $data = $request->fetch(PDO::FETCH_OBJ);
+}
+echo '</tbody></table>';
+
+
+/*** close the database connection ***/
+
+$pdo = null;
+
+?>
