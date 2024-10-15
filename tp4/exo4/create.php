f8a3e701ad5e484cf25a21f3a2de915f2c5b1ebb
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2024-10-15 11:13:29 +0200

    feat: facto code config

--- tp4/exo4/create.php
+++ tp4/exo4/create.php
@@ -1,23 +1,6 @@
 <?php
     require_once('config.php');
 
-    $connectionString = "mysql:host=". _MYSQL_HOST;
-
-    if(defined('_MYSQL_PORT'))
-        $connectionString .= ";port=". _MYSQL_PORT;
-
-    $connectionString .= ";dbname=" . _MYSQL_DBNAME;
-    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
-
-    $pdo = NULL;
-    try {
-        $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $erreur) {
-        echo 'Erreur : '.$erreur->getMessage();
-    }
-
     if(isset($_POST['login']) && !empty($_POST['login'])){
         $sqlAdd = "INSERT INTO users (id, login, email) VALUES (NULL, :login, :email)";
         $requestAdd = $pdo->prepare($sqlAdd);
